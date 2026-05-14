128a18806069314aca348de67c9238c43f598fb2
Benteqi <benedykt.cyran@gmail.com>
2026-05-14 22:14:35 +0200

    Krótkofalówka

---
+++ scripts/scena_3_radio.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var popup = $"Radio popup"
+@onready var label = $"Radio popup/Label"
+# Called when the node enters the scene tree for the first time.
+func show_radio_message(text : String):
+	label.text = text
+	popup.visible = true
+	await get_tree().create_timer(5.0).timeout
+	popup.visible = false
+	
+func _ready() -> void:
+	await get_tree().create_timer(10.0).timeout
+	show_radio_message("No name test message for testing")
