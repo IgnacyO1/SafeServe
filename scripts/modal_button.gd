bf988e70582b39db86d7d62b7d8855be0666d890
Benteqi <benedykt.cyran@gmail.com>
2026-05-15 23:08:18 +0200

    Nowe UI scena 1

--- scripts/modal_button.gd
+++ scripts/modal_button.gd
@@ -1,12 +1,6 @@
 extends TextureButton
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
@@ -14,3 +8,6 @@ func _on_press():
 	get_parent().visible = false
 func _begin_game():
 	get_tree().current_scene.map_container.map_locked = false
+func _play_message():
+	
+	get_tree().change_scene_to_file("res://scenes/scena_2.tscn")
