bbb63bd528086cd1b55f1eac29ef939e165de06b
Janek <jan.j.popowicz@gmail.com>
2026-06-07 20:46:11 +0200

    czy już działa?

--- scripts/traffic_manager_for_police_multiplayer.gd
+++ scripts/traffic_manager_for_police_multiplayer.gd
@@ -23,10 +23,20 @@ func _process(_delta):
 
 func spawn_boss_car():
 	if uciekinier_scene == null: return
-	
+
+	var world = get_node("../")
 	uciekinier = uciekinier_scene.instantiate()
 	uciekinier.name = "CyberkrabBoss" # <- To musi być pierwsze!
-	get_node("../").add_child(uciekinier) # <- Dopiero teraz wrzucamy do drzewa sieciowego
-	
+
+	var spawn_point = Vector2.ZERO
+	if "fixed_path" in uciekinier and not uciekinier.fixed_path.is_empty():
+		spawn_point = uciekinier.fixed_path[0]
+
+	# Ustawiam pozycję przed dodaniem do drzewa, żeby początkowy spawn sieciowy
+	# używał dokładnie pierwszego wektora z fixed_path.
+	uciekinier.position = spawn_point - world.global_position
+	world.add_child(uciekinier) # <- Dopiero teraz wrzucamy do drzewa sieciowego
+
 	uciekinier.setup([], map_manager, false)
+	uciekinier.global_position = spawn_point
 	print("[SERWER] Spawnowano uciekiniera zsynchronizowanego przez ENet!")
